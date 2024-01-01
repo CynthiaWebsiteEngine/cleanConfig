f3825ed3d1bcffb9ffab90ee4999528f53ae3402
Mar <mar@strawmelonjuice.com>
2024-01-01 03:35:31 +0100

    Fix some issues in client side script

--- cynthiaFiles/assets/scripts/client.js
+++ cynthiaFiles/assets/scripts/client.js
@@ -6,15 +6,14 @@ This script is embedded into any page Cynthia serves, always just before the clo
 
 
 setInterval(() => {
-    var elements = document.getElementsByClassName('unparsedtimestamp');
+    const elements = document.getElementsByClassName('unparsedtimestamp');
     for (let i = elements.length - 1; i >= 0; i--) {
-        let timestamp = elements[i].innerHTML;
+        let timestamp = parseInt(elements[i].textContent);
         console.log("Parsing timestamp.");
         const jstimestamp = timestamp * 1000;
         const dateObject = new Date(jstimestamp);
         const data = dateObject.toLocaleString();
-        const date = data.substring(0, data.length - 3);
-        elements[i].innerHTML = date;
+        elements[i].textContent = data.substring(0, data.length - 3);
         elements[i].classList.remove('unparsedtimestamp');
     }
 }, 100);
@@ -34,6 +33,7 @@ function mobileorientation() {
         "Could not determine 'mobilescreen()' from css value '" + csssays + "'."
     );
 }
+
 if (document.getElementById("cynthiapageinfoshowdummyelem") != null) {
     let pageinfosidebarelem = document.getElementById(
         "cynthiapageinfoshowdummyelem"
@@ -48,21 +48,21 @@ if (document.getElementById("cynthiapageinfoshowdummyelem") != null) {
     if (
         typeof pagemetainfo.author.thumbnail !== "undefined"
     ) {
-        authorthumbnail = `<img style="width: 2.5em" src="${pagemetainfo.author.thumbnail}">`;
+        authorthumbnail = `<img alt="Author thumbnail" style="width: 2.5em" src="${pagemetainfo.author.thumbnail}">`;
     }
     let dates = "";
     if (typeof pagemetainfo.dates !== "undefined") {
-        if (pagemetainfo.dates.published == pagemetainfo.dates.altered || typeof pagemetainfo.dates.altered == "undefined") {
-            dates = `<li>Posted: <span class="unparsedtimestamp">${(new Date((pagemetainfo.dates.published) * 1000).toLocaleString())}</span></li>`
+        if (pagemetainfo.dates.published === pagemetainfo.dates.altered || typeof pagemetainfo.dates.altered == "undefined") {
+            dates = `<li>Posted: <span class="preparsedtimestamp">${(new Date((pagemetainfo.dates.published) * 1000).toLocaleString())}</span></li>`
         } else {
             dates = `
-    <li>Posted: <span class="unparsedtimestamp">${(new Date((pagemetainfo.dates.published) * 1000).toLocaleString())}</span></li>
-    <li>Edited: <span class="unparsedtimestamp">${(new Date((pagemetainfo.dates.altered) * 1000).toLocaleString())}</span></li>
+    <li>Posted: <span class="preparsedtimestamp">${(new Date((pagemetainfo.dates.published) * 1000).toLocaleString())}</span></li>
+    <li>Edited: <span class="preparsedtimestamp">${(new Date((pagemetainfo.dates.altered) * 1000).toLocaleString())}</span></li>
     `
         }
     }
     pageinfosidebarelem.innerHTML = `
-    <span class="not-on-mobile" style="position:absolute;right:0;top:0px;font-size: 3em; cursor: pointer; ">⇙</span>
+    <span class="not-on-mobile" style="position:absolute;right:0;top:0;font-size: 3em; cursor: pointer; ">&#8665;</span>
     <p class="pageinfo-title">${pagemetainfo.title}</p>
     <ul>
       <li>Author: ${authorthumbnail} ${pagemetainfo.author.name}</li>
@@ -70,36 +70,38 @@ if (document.getElementById("cynthiapageinfoshowdummyelem") != null) {
       </ul>
     <p class="pageinfo-shortversion">${pagemetainfo.short}</p>
       `;
+
     function pageinfosidebar_rollup() {
         document.getElementById("pageinfosidebar").style.overflow = "hidden";
-        document.getElementById("pageinfosidebar").style.width = "0px";
-        document.getElementById("pageinfosidebar").style.maxHeight = "310px";
+        document.getElementById("pageinfosidebar").style.width = "0";
+        document.getElementById("pageinfosidebar").style.maxHeight = "310";
         document.getElementById("pageinfosidebartoggle").style.display = "";
         setTimeout(() => {
-            document.getElementById("pageinfosidebartoggle").style.width = "40px";
-        }, "1700");
+            document.getElementById("pageinfosidebartoggle").style.width = "40";
+        }, 1700);
         setTimeout(() => {
             document.getElementById("pageinfosidebartoggle").style.padding = "8px";
-        }, "1800");
+        }, 1800);
     }
+
     function pageinfosidebar_rollout() {
         document.getElementById("pageinfosidebar").style.overflow = "";
         document.getElementById("pageinfosidebar").style.opacity = "100%";
         document.getElementById("pageinfosidebartoggle").style.overflow = "hidden";
-        document.getElementById("pageinfosidebartoggle").style.width = "0px";
-        document.getElementById("pageinfosidebartoggle").style.padding = "0px";
+        document.getElementById("pageinfosidebartoggle").style.width = "0";
+        document.getElementById("pageinfosidebartoggle").style.padding = "0";
         setTimeout(() => {
             document.getElementById("pageinfosidebar").style.width = "";
-        }, "1800");
+        }, 1800);
         setTimeout(() => {
             document.getElementById("pageinfosidebar").style.height = "";
-        }, "1900");
+        }, 1900);
     }
 
     if (mobileorientation() || window.innerHeight < 350) {
         setTimeout(() => {
             pageinfosidebar_rollup();
-        }, "6000");
+        }, 6000);
         document.getElementById("pageinfosidebar").style.opacity = "100%";
     } else {
         document.getElementById("pageinfosidebar").style.opacity = "30%";
@@ -110,12 +112,4 @@ if (document.getElementById("cynthiapageinfoshowdummyelem") != null) {
             .getElementById("pageinfosidebar")
             .setAttribute("onmouseout", "this.style.opacity = '30%'");
     }
-    $("#pageinfosidebar").on("click", function (e) {
-        if (e.target == document.getElementById("dummyauthorthumbnail")) {
-            GhostAuthorThumbnailExpand();
-            return;
-        }
-
-        pageinfosidebar_rollup();
-    });
 }
